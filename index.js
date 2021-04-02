b42f3f25fc2ba24ea86f3ec9582ad5ae3c2cdfb3
Damian <spearsda16@gmail.com>
2021-04-02 12:47:57 -0400

    done with assingment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+   
+    let blocks;
+   blocks = Math.abs(street-42);
+
+   return blocks
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    
+    let feetDist;
+    feetDist = distanceFromHqInBlocks(blockNumber) * 264
+
+
+    return feetDist;
+}
+
+
+function distanceTravelledInFeet(start, end) {
+
+    let travel;
+    travel = Math.abs(start-end) * 264;
+
+    return travel;
+}
+
+function calculatesFarePrice(start, destination) {
+
+    let travel;
+    travel = Math.abs(start-destination) * 264;
+
+    let price;
+
+    if (travel < 400) { 
+
+        price = 0;
+    }
+    else if (travel > 400 && travel < 2000) {
+        price = (travel-400) * 0.02;
+    }
+    else if (travel > 2000 && travel <2500) {
+        price = 25;
+    }
+    else if (travel > 2500) {
+        price = 'cannot travel that far'
+    }
+
+    return price;
+}
+
+
+
